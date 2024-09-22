e4dc44548b164b4060b6b00fc10a73e127b3e799
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-09-22 21:44:21 +0900

    [#2] 프로젝트 초기 설정

---
+++ src/main/java/com/bilyeocho/BilyeoChoApplication.java
@@ -0,0 +1,13 @@
+package com.bilyeocho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilyeoChoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilyeoChoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bilyeocho/BilyeoChoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilyeocho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilyeoChoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
