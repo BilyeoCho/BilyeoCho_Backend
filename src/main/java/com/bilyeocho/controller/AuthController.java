cb47b785c943efa9030a66b0313ab015dfbbe50d
sungw0o <sungwoo515@gmail.com>
2024-10-30 21:38:01 +0900

    fix : route api add [#3]

--- src/main/java/com/bilyeocho/controller/AuthController.java
+++ src/main/java/com/bilyeocho/controller/AuthController.java
@@ -12,11 +12,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "인증", description = "유저 회원가입, 로그인, 토큰 재발급")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class AuthController {
 
     private final AuthService authService;
