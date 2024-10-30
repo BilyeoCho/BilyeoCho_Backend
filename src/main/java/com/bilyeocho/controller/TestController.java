58b617d5048408294d358401af418242523f17ea
sungw0o <sungwoo515@gmail.com>
2024-10-30 16:44:53 +0900

    fix : docker compose update [#3]

--- src/main/java/com/bilyeocho/controller/TestController.java
+++ src/main/java/com/bilyeocho/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Operation(summary = "테스트 페이지", description = "API 설명.")
+    @Operation(summary = "테스트 페이지!", description = "API 설명.")
     @GetMapping("/")
     public String test() {
         return "Hello World~";
