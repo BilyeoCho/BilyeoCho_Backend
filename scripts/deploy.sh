1897171032d1cd11fb0004ad6d4897695db43437
sungw0o <sungwoo515@gmail.com>
2024-10-30 16:46:36 +0900

    fix : docker compose update [#3]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/app
 DOCKER_APP_NAME=bilyeocho
 
 # 실행 중인 blue가 있는지 확인
-EXIST_BLUE=$(sudo docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작 일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
@@ -18,14 +18,14 @@ if [ -z "$EXIST_BLUE" ]; then
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
 
   # docker-compose.blue.yml 파일을 사용하여 connectdog-blue 프로젝트의 컨테이너를 빌드하고 실행
-  BUILD_OUTPUT=$(sudo docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build 2>&1)
+  BUILD_OUTPUT=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build 2>&1)
   echo "$BUILD_OUTPUT" >> /home/ec2-user/deploy.log
 
   # 30초 동안 대기
   sleep 30
 
   # blue가 문제 없이 배포 되었는지 현재 실행여부를 확인한다
-  BLUE_HEALTH=$(sudo docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+  BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
   # blue가 현재 실행중이지 않다면 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
   if [ -z "$BLUE_HEALTH" ]; then
@@ -35,7 +35,7 @@ if [ -z "$EXIST_BLUE" ]; then
     echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
 
     # docker-compose.green.yml 파일을 사용하여 connectdog-green 프로젝트의 컨테이너를 중지
-    sudo docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+    sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 
     # 사용하지 않는 이미지 삭제
     sudo docker image prune -af
@@ -46,12 +46,12 @@ if [ -z "$EXIST_BLUE" ]; then
 # blue가 실행중이면 green up
 else
   echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-  BUILD_OUTPUT=$(sudo docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build 2>&1)
+  BUILD_OUTPUT=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build 2>&1)
   echo "$BUILD_OUTPUT" >> /home/ec2-user/deploy.log
 
   sleep 30
   # green이 문제 없이 배포 되었는지 현재 실행여부를 확인한다
-  GREEN_HEALTH=$(sudo docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
+  GREEN_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
 
   # green이 현재 실행중이지 않다면 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
   if [ -z "$GREEN_HEALTH" ]; then
@@ -61,7 +61,7 @@ else
       echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
 
       # docker-compose.blue.yml 파일을 사용하여 connectdog-green 프로젝트의 컨테이너를 중지
-      sudo docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+      sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
 
       # 사용하지 않는 이미지 삭제
       sudo docker image prune -af
